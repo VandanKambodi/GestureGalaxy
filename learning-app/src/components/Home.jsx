42e17e9c0535cba8ff051b1ca650188599c5671b
Vandan Kambodi <vandankambodi27@gmail.com>
2025-03-02 09:01:37 +0530

    update overflow

--- learning-app/src/components/Home.jsx
+++ learning-app/src/components/Home.jsx
@@ -23,6 +23,96 @@ const Home = () => {
             </div>
           </div>
         </div>
+        <div className="basic">
+          <div className="part">
+            <div className="left">
+              <h1 className="head">BASIC</h1>
+              <p className="para">Some basics for building language</p>
+              <Link className="b1" to="/basic">Learn Basic</Link>
+              <Link className="b2" to="/basic">Video Tutorial</Link>
+              <Link className="b3" to="/basic">Reference</Link>
+              <Link className="b4" to="/certificate">Give Quiz</Link>
+            </div>
+            <div className="right">
+                <img src="./logo.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="basic">
+          <div className="part">
+            <div className="left">
+              <h1 className="head">BASIC</h1>
+              <p className="para">Some basics for building language</p>
+              <Link className="b1" to="/basic">Learn Basic</Link>
+              <Link className="b2" to="/basic">Video Tutorial</Link>
+              <Link className="b3" to="/basic">Reference</Link>
+              <Link className="b4" to="/certificate">Give Quiz</Link>
+            </div>
+            <div className="right">
+                <img src="./logo.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="basic">
+          <div className="part">
+            <div className="left">
+              <h1 className="head">BASIC</h1>
+              <p className="para">Some basics for building language</p>
+              <Link className="b1" to="/basic">Learn Basic</Link>
+              <Link className="b2" to="/basic">Video Tutorial</Link>
+              <Link className="b3" to="/basic">Reference</Link>
+              <Link className="b4" to="/certificate">Give Quiz</Link>
+            </div>
+            <div className="right">
+                <img src="./logo.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="basic">
+          <div className="part">
+            <div className="left">
+              <h1 className="head">BASIC</h1>
+              <p className="para">Some basics for building language</p>
+              <Link className="b1" to="/basic">Learn Basic</Link>
+              <Link className="b2" to="/basic">Video Tutorial</Link>
+              <Link className="b3" to="/basic">Reference</Link>
+              <Link className="b4" to="/certificate">Give Quiz</Link>
+            </div>
+            <div className="right">
+                <img src="./logo.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="basic">
+          <div className="part">
+            <div className="left">
+              <h1 className="head">BASIC</h1>
+              <p className="para">Some basics for building language</p>
+              <Link className="b1" to="/basic">Learn Basic</Link>
+              <Link className="b2" to="/basic">Video Tutorial</Link>
+              <Link className="b3" to="/basic">Reference</Link>
+              <Link className="b4" to="/certificate">Give Quiz</Link>
+            </div>
+            <div className="right">
+                <img src="./logo.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="basic">
+          <div className="part">
+            <div className="left">
+              <h1 className="head">BASIC</h1>
+              <p className="para">Some basics for building language</p>
+              <Link className="b1" to="/basic">Learn Basic</Link>
+              <Link className="b2" to="/basic">Video Tutorial</Link>
+              <Link className="b3" to="/basic">Reference</Link>
+              <Link className="b4" to="/certificate">Give Quiz</Link>
+            </div>
+            <div className="right">
+                <img src="./logo.png" alt="" />
+            </div>
+          </div>
+        </div>
     </section>
   )
 }
