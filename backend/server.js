784dc5539e524ecc7eccf45e5d85f493b319cf4c
Vandan Kambodi <vandankambodi27@gmail.com>
2025-04-30 21:13:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,16 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
+// Handle 404 errors
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Resource not found" });
+});
+
+// Handle other errors (optional)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Server error" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
